d433be8fb4d0c4b0df3209bb478e3566ac6a33dc
Richmelody <chigs1million@gmail.com>
2023-04-21 17:32:41 +0000

    corrections

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,94 @@
-# Installs a Nginx server
+# Install and configure Nginx
+class nginx {
+  package { 'nginx':
+    ensure => installed,
+  }
 
-exec {'install':
-  provider => shell,
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/luischaparroc permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+  }
+
+  file { '/var/www/html/index.nginx-debian.html':
+    ensure  => file,
+    content => 'Hello World!',
+    require => Package['nginx'],
+  }
+
+  file { '/etc/nginx/sites-available/default':
+    ensure  => file,
+    content => template('nginx/default.conf.erb'),
+    notify  => Service['nginx'],
+  }
 }
+
+# Configure default Nginx site
+class nginx::default_site {
+  file { '/var/www/html/error_404.html':
+    ensure => file,
+    content => 'Ceci n\'est pas une page',
+    require => Class['nginx'],
+  }
+
+  nginx::resource::location { 'redirect_me':
+    ensure    => present,
+    location  => '/redirect_me',
+    rewrite   => 'https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent',
+    require   => Class['nginx'],
+  }
+}
+
+# Resource definition for Nginx location block
+define nginx::resource::location (
+  $ensure,
+  $location,
+  $rewrite = undef,
+) {
+  $config = {
+    'location' => $location,
+    'rewrite'  => $rewrite,
+  }
+
+  nginx::config { "location_${location}":
+    ensure => $ensure,
+    content => template('nginx/location.conf.erb'),
+    config  => $config,
+    require => Class['nginx'],
+  }
+}
+
+# Template for default Nginx site
+file { '/etc/nginx/sites-available/default':
+  ensure => 'link',
+  target => '/etc/nginx/sites-available/default.conf',
+}
+
+file { '/etc/nginx/sites-available/default.conf':
+  ensure  => file,
+  content => template('nginx/default.conf.erb'),
+}
+
+# Template for Nginx location block
+file { '/etc/nginx/conf.d/location.conf':
+  ensure => absent,
+}
+
+file { '/etc/nginx/location.conf':
+  ensure  => file,
+  content => template('nginx/location.conf.erb'),
+}
+
+# Set up Nginx to listen on port 80
+class { 'nginx': }
+
+# Restart Nginx if configuration changes
+notify { 'nginx-restart':
+  subscribe => [
+    File['/var/www/html/index.nginx-debian.html'],
+    File['/etc/nginx/sites-available/default'],
+    File['/etc/nginx/sites-available/default.conf'],
+    File['/etc/nginx/location.conf'],
+  ],
+  notify    => Service['nginx'],
+}
+
