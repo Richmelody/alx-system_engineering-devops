f055f06a61743087a2cd4440e0efa3def91462cb
Richmelody <chigs1million@gmail.com>
2023-05-10 16:01:52 +0000

    advance api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,39 +1,27 @@
 #!/usr/bin/python3
-from requests import get
-from sys import argv
+"""
+Function that queries the Reddit API and returns
+the number of subscribers for a given subreddit.
+"""
+import requests
+import sys
 
-# get subs
 
 def number_of_subscribers(subreddit):
-    """Get number of subscribers for a given subreddit."""
-    head = {'User-Agent': 'Dan Kazam'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=head, allow_redirects=False)
+    """ Queries to Reddit API """
+    u_agent = 'Mozilla/5.0'
 
-    if response.status_code == 302:
-        print("Error: Invalid subreddit '{}'".format(subreddit))
+    headers = {
+        'User-Agent': u_agent
+    }
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code != 200:
         return 0
-    elif response.status_code != 200:
-        print("Error: {} - Could not retrieve data for /r/{}".format(response.status_code, subreddit))
-        return None
-
-    try:
-        count = response.json().get('data').get('subscribers')
-        return count
-    except:
-        print("Error: Could not retrieve subscriber count for /r/{}".format(subreddit))
-        return None
-
-
-if __name__ == "__main__":
-    subreddits = argv[1:]
-    subscriber_counts = {}
-
-    for subreddit in subreddits:
-        count = number_of_subscribers(subreddit)
-        if count is not None:
-            subscriber_counts[subreddit] = count
-
-    for subreddit, count in sorted(subscriber_counts.items()):
-        print("{}: {}".format(subreddit, count))
-
+    dic = res.json()
+    if 'data' not in dic:
+        return 0
+    if 'subscribers' not in dic.get('data'):
+        return 0
+    return res.json()['data']['subscribers']
