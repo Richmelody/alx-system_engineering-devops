cc7aa4e90115f75b05110857c146b581030e2f7c
Richmelody <chigs1million@gmail.com>
2023-05-10 15:49:27 +0000

    advance api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-# get subs
 from requests import get
 from sys import argv
 
+# get subs
 
 def number_of_subscribers(subreddit):
     """Get number of subscribers for a given subreddit."""
